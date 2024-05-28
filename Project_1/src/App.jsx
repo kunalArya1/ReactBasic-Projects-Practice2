c9a52e2cb847741c5347d25947c6b1132183857c
kunalArya1 <kunalkrraj220@gmail.com>
2024-05-28 09:53:14 +0530

    Project 1 Completed

---
+++ Project_1/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Greeting from "./Component/Greeting";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Greeting App</h1>
+      <Greeting name="Kunal Kumar Arya" />
+    </div>
+  );
+};
+
+export default App;
