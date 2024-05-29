15832efb6ad2b481436e73dd5112862aaf1055c8
kunalArya1 <kunalkrraj220@gmail.com>
2024-05-29 14:38:31 +0530

    Project 9 Created

---
+++ Project_9/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+/** Build a React component called "ColorPicker" that displays a list of colors as buttons. On click of each button show the HEX code for that color, just below each button.
+ 
+Data:
+ const red = '#EE4B2B'
+ const blue = '#89CFF0'
+ const green = '#7FFFD4' */
+ 
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
