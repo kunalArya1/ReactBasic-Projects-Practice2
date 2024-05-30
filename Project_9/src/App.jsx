cb0870e4141bd035b2a44282d63258d4ee316321
kunalArya1 <kunalkrraj220@gmail.com>
2024-05-30 11:29:37 +0530

    Project 9 Completed

--- Project_9/src/App.jsx
+++ Project_9/src/App.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from "react";
+import ColorPicker from "./Component/ColorPicker";
 
 /** Build a React component called "ColorPicker" that displays a list of colors as buttons. On click of each button show the HEX code for that color, just below each button.
  
@@ -6,11 +7,13 @@ Data:
  const red = '#EE4B2B'
  const blue = '#89CFF0'
  const green = '#7FFFD4' */
- 
+
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <ColorPicker />
+    </div>
+  );
+};
 
-export default App
+export default App;
--- Project_9/src/Component/ColorPicker.jsx
+++ Project_9/src/Component/ColorPicker.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 /** Build a React component called "ColorPicker" that displays a list of colors as buttons. On click of each button show the HEX code for that color, just below each button.
  
@@ -8,10 +8,29 @@ Data:
  const green = '#7FFFD4' */
 
 const ColorPicker = () => {
+  const [selectedIndex, setselectedIndex] = useState(null);
+  const data = [
+    { color: "#EE4B2B", name: "Red" },
+    { color: "#89CFF0", name: "Blue" },
+    { color: "#7FFFD4", name: "Green" },
+  ];
   return (
-    <div>
-      <h1>ColorPicker App </h1>
-      <button></button>
+    <div className=" text-center">
+      <h1 className=" font-bold text-center m-5">ColorPicker App </h1>
+      <div>
+        {data.map((color, index) => (
+          <>
+            <button
+              onClick={() => setselectedIndex(index)}
+              className=" px-4 py-2 m-4 border rounded-md border-black "
+            >
+              {color.name}
+            </button>
+            <br />
+            {selectedIndex === index && <p>{color.color}</p>}
+          </>
+        ))}
+      </div>
     </div>
   );
 };
